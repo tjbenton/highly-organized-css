926a067ddbef1086ca3a165dccb75ee600391253
Tyler Benton <tjbenton21@gmail.com>
2016-06-03 10:56:43 -0400

    added specificity stuff

--- flyfile.js
+++ flyfile.js
@@ -2,14 +2,40 @@ import path from 'path'
 import livereload from 'livereload'
 import { spawn } from 'child_process'
 import to from 'to-js'
-// import simpleserver from 'http-server'
+import fs from 'fs-extra-promisify'
+
+const specificity_path = path.join(__dirname, 'css-libs', 'css-libs.json')
+
+export async function specificity() {
+  const result = {}
+  let data = []
+  const keys = []
+  const files = await fs.readdir(path.join(__dirname, 'css-libs'))
+
+  for (let file of files) {
+    if (file.indexOf('.css') > -1) {
+      keys.push(file.split('.')[0])
+      data.push(fs.readFile(path.join(__dirname, 'css-libs', file)))
+    }
+  }
+
+  data = await Promise.all(data)
+  for (let [ i, item ] of data.entries()) {
+    result[keys[i]] = item + ''
+  }
+
+  await fs.outputJson(specificity_path, result)
+
+}
+
 export async function jade() {
   await this
     .source(path.join('views', 'index.jade'))
     .jade({
       base: 'views',
       pretty: true,
-      to
+      to,
+      css_libs: await fs.readJson(specificity_path)
     })
     .target('.', { depth: 2 })
 }
@@ -29,9 +55,9 @@ export async function styles() {
 }
 
 export async function watch() {
+  await this.watch(path.join('css-libs', '**', '*.css'), 'specificity', 'jade')
   await this.watch(path.join('views', '**', '*.jade'), 'jade');
   await this.watch(path.join('lib', 'stylus', '**', '*.styl'), 'styles')
-
   const httpServer = spawn(path.join('.', 'node_modules', '.bin', 'http-server'), [])
   let should_log = true
   let i = 0
